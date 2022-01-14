53935b7eb0e5701769b120e27d89ca81b605ba0a
CivilTT <civiltt.ss@gmail.com>
2022-01-14 15:42:40 +0900

    make new Classes

---
+++ Server_GUI2/System/Server/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_GUI2
+{
+    class Server
+    {
+        private World world;
+
+        public Server(World _world)
+        {
+            world = _world;
+        }
+
+        public void Run()
+        {
+
+        }
+
+    }
+}
