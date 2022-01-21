c05cb983268efa3897b5fbcca0efc4264aeed762
txkodo <kodo.hsmkai@gmail.com>
2022-01-21 17:06:13 +0900

    changed versionfactory

--- Server_GUI2/Develop/Server/Version/VersionFactory.cs
+++ Server_GUI2/Develop/Server/Version/VersionFactory.cs
@@ -13,6 +13,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using MW = ModernWpf;
+using Newtonsoft.Json;
 
 namespace Server_GUI2
 {
@@ -21,11 +22,9 @@ namespace Server_GUI2
         private readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         readonly WebClient wc = new WebClient();
 
-        public static VersionFactory _instance = new VersionFactory();
+        private static VersionFactory _instance = new VersionFactory();
 
         public List<Version> allVersions = new List<Version>();
-
-        public List<Version> installedVersions = new List<Version>();
 
         public static Version activeVer = null;
 
@@ -35,16 +34,44 @@ namespace Server_GUI2
             return _instance;
         }
 
-        public VersionFactory()
+        private VersionFactory()
         {
             LoadAllVersions();
-            LoadImported();
+            // LoadImported();
+        }
+
+        public List<Version> ExistingVersions
+        {
+            get
+            {
+                return allVersions.Where(version => version.Exists).ToList();
+            }
+        }
+        public List<Version> SpigptVersions
+        {
+            get
+            {
+                return allVersions.Where(version => version is SpigotVersion).ToList();
+            }
+        }
+        public List<Version> VanillaAllVersions
+        {
+            get
+            {
+                return allVersions.Where(version => version is VanillaVersion).ToList();
+            }
+        }
+        public List<Version> VanillaReleaseVersions
+        {
+            get
+            {
+                return allVersions.Where(version => version is VanillaVersion &&  (version as VanillaVersion).IsRelease ).ToList();
+            }
         }
 
         /// <summary>
         /// マイクラのバージョン一覧を取得
         /// </summary>
-        /// <returns>一覧のリストを返す。取得に失敗した場合はnullを返す</returns>
         public void LoadAllVersions()
         {
             logger.Info("Import new Version List");
@@ -53,20 +80,20 @@ namespace Server_GUI2
             string errorMessage =
                     "Minecraftのバージョン一覧の取得に失敗しました。\n" +
                     "新しいバージョンのサーバーの導入はできません";
-            dynamic root = ReadContents.ReadJson(url, errorMessage);
+            dynamic root = ReadContents.ReadJson<VanillaVersonsJson>(url, errorMessage);
             if (root == null)
             {
                 logger.Info("Missing Versions List");
-
                 // TODO: version_manifest_v2.jsonを保持しておき、インターネットから読み込めないときにはこれを利用する
                 // これもないときにはServerStarterは起動できないこととする（そもそもインターネットに接続していない状態でサーバーを立てたいか？）
                 return;
             }
 
             List<string> spigotList = GetSpigotVersions();
+            VanillaVersonsJson vanillaversions = root;
 
-            string latestRelease = root.latest.release;
-            string latestSnapShot = root.latest.snapshot;
+            string latestRelease = vanillaversions.latest.release;
+            string latestSnapShot = vanillaversions.latest.snapshot;
 
             string id = "";
             int i = 0;
@@ -74,27 +101,31 @@ namespace Server_GUI2
             // バージョン1.2.5以前はマルチサーバーが存在しない
             while (id != "1.2.5")
             {
-                id = root.versions[i].id;
-                string downloadURL = root.versions[i].url;
-                string type = root.versions[i].type;
-                bool hasSpigot = (spigotList != null) && spigotList.Contains(id);
+                VanillaVersonJson version = vanillaversions.versions[i];
+                id = version.id;
+                string downloadURL = version.url;
+                string type = version.type;
+                bool hasSpigot = (spigotList?.Contains(id)) ?? false;
                 bool isRelease = type == "release";
-                bool isLatest = id == latestRelease || id == latestSnapShot;
+                // bool isLatest = id == latestRelease || id == latestSnapShot;
 
-                allVersions.Add(new Version(id, downloadURL, hasSpigot, isRelease, isLatest));
+                allVersions.Add(new VanillaVersion(id, downloadURL, isRelease, hasSpigot));
 
                 i++;
             }
 
             // 最新バージョンがreleaseの際にはsnapshotも同じため、特例としてリストの先頭に挿入する処理を行う
+            // TODO: 謎処理 hasSpigotはtrueか？
             if (latestRelease == latestSnapShot)
             {
-                id = root.versions[0].id;
-                string downloadURL = root.versions[0].url;
-                Version _snapshot = new Version(id, downloadURL, false, false, true);
+                VanillaVersonJson version = vanillaversions.versions[0];
+                id = version.id;
+                string downloadURL = version.url;
+                Version _snapshot = new VanillaVersion(id, downloadURL, false, true);
                 allVersions.Insert(1, _snapshot);
             }
         }
+
         private List<string> GetSpigotVersions()
         {
             string url = "https://hub.spigotmc.org/versions/";
@@ -130,48 +161,50 @@ namespace Server_GUI2
         }
 
         /// <summary>
-        /// すでにインストールされているバージョンの一覧を取得
+        /// バージョンのリストを引数に取る
+        /// バージョンのリストに含まれないが、すでにインストールされているバージョンがあった場合にそのリストに追加(オフライン時等)
+        /// manifest.jsonがない場合はそもそも実行しないので必要なし。
         /// </summary>
-        public void LoadImported()
-        {
-            logger.Info("Getting local Versions");
+        // private List<Version> CheckImported(List<Version> versions)
+        // {
+        // logger.Info("Getting local Versions");
+        // 
+        //     foreach (string dirName in Directory.GetDirectories(SetUp.DataPath, "*", SearchOption.TopDirectoryOnly))
+        //     {
+        // string verName = Path.GetFileName(dirName);
+        // 
+        // bool isSpigot = verName.Contains("Spigot");
+        // verName = isSpigot ? verName.Substring(7) : verName;
+        // 
+        // newしないと参照渡しになってしまうため
+        // Version _ver = versions.Find(x => x.Name == verName);
+        // 
+        //         if (_ver != null)
+        //         {
+        // Version ver = new Version(_ver.Name, _ver.downloadURL, _ver.hasSpigot, _ver.isRelease, _ver.isLatest, !isSpigot);
+        // installedVersions.Add(ver);
+        //     }
+        // }
+    }
 
-            foreach (string dirName in Directory.GetDirectories(SetUp.DataPath, "*", SearchOption.TopDirectoryOnly))
-            {
-                string verName = Path.GetFileName(dirName);
 
-                bool isSpigot = verName.Contains("Spigot");
-                verName = isSpigot ? verName.Substring(7) : verName;
+    /// <summary>
+    /// バージョンの新規作成
+    /// </summary>
+    /// <returns>作成したバージョンの情報</returns>
+    //public Version Create(bool isVanila=true)
+    //{
+    //    if(isVanila)
+    //    {
+    //        //pass
+    //    }
+    //    else
+    //    {
 
-                // newしないと参照渡しになってしまうため
-                Version _ver = allVersions.Find(x => x.Name == verName);
+    //    }
+    //}
 
-                if (_ver != null)
-                {
-                    Version ver = new Version(_ver.Name, _ver.downloadURL, _ver.hasSpigot, _ver.isRelease, _ver.isLatest, !isSpigot);
-                    installedVersions.Add(ver);
-                }
-            }
-        }
-
-
-        /// <summary>
-        /// バージョンの新規作成
-        /// </summary>
-        /// <returns>作成したバージョンの情報</returns>
-        //public Version Create(bool isVanila=true)
-        //{
-        //    if(isVanila)
-        //    {
-        //        //pass
-        //    }
-        //    else
-        //    {
-
-        //    }
-        //}
-
-        public void Remove(Version version) { }
+    public void Remove(Version version) { }
 
         //public string[] getVersionNames() { }
 
@@ -182,5 +215,42 @@ namespace Server_GUI2
         //Version[] allSpigotVersions
         //Version activeVersion
         //string[] getVersionNames()
+    }
+
+
+    public class VanillaVersonsJson
+    {
+        public static int LatestJsonVer = 1;
+
+        [JsonProperty("latest")]
+        //"latest": {
+        //    "release": "1.18.1",
+        //    "snapshot": "22w03a"
+        //}
+        public VanillaLatestVersonJson latest;
+
+        [JsonProperty("versions")]
+        public List<VanillaVersonJson> versions;
+
+    }
+    public class VanillaVersonJson
+    {
+        [JsonProperty("id")]
+        public string id;
+
+        [JsonProperty("url")]
+        public string url;
+        
+        [JsonProperty("type")]
+        public string type;
+    }
+
+    public class VanillaLatestVersonJson
+    {
+        [JsonProperty("release")]
+        public string release;
+
+        [JsonProperty("snapshot")]
+        public string snapshot;
     }
 }
