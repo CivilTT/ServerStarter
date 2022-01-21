7856a29d96f736aae5d53de2a43732c1b72930f9
CivilTT <civiltt.ss@gmail.com>
2022-01-21 15:32:32 +0900

    Download VanilaVersion

--- Server_GUI2/Develop/Server/Version/Version.cs
+++ Server_GUI2/Develop/Server/Version/Version.cs
@@ -7,22 +7,19 @@ using System.Threading.Tasks;
 using System.IO;
 
 using log4net;
+using System.Windows;
+using MW = ModernWpf;
+using System.Net;
 
 namespace Server_GUI2
 {
     public class Version:IComparable<Version>
     {
-        private readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public static WebClient wc = new WebClient();
 
         public string Name;
-        private string Path
-        {
-            get
-            {
-                return $@"{MainWindow.Data_Path}\{Name}\";
-            }
-        }
-
+        public virtual string Path { get; }
         public bool Exists
         {
             get
@@ -30,6 +27,7 @@ namespace Server_GUI2
                 return Directory.Exists(Path);
             }
         }
+
 
         //public string downloadURL;
 
@@ -48,36 +46,129 @@ namespace Server_GUI2
             Name = name;
         }
 
-        public virtual void DownloadVersion() { }
+        public virtual void SetNewVersion()
+        {
+            logger.Info("There are already new version, or not");
+            if (Directory.Exists(Path))
+            {
+                logger.Info("There are already new version");
+                return;
+            }
+
+            logger.Info("Download server.jar");
+            if (Data_list.Version == "")
+            {
+                MessageBoxResult? result = MW.MessageBox.Show("導入するサーバーのバージョンが選択されていません。\r\nサーバーのバージョンを選択をした上で再度「Run」を押してください。", "Server Starter", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                if (result == MessageBoxResult.OK)
+                {
+                    System.Windows.Forms.Application.Restart();
+                }
+                throw new ArgumentException("Did not select opening version");
+            }
+        }
         public virtual void Remove() { }
 
         // 比較可能にする
         public virtual int CompareTo(Version obj)
         {
+            // TODO: Versionの比較について実装する
             throw new NotImplementedException();
         }
     }
 
     public class VanillaVersion: Version
     {
+        public override string Path
+        {
+            get
+            {
+                return $@"{MainWindow.Data_Path}\{Name}\";
+            }
+        }
+
+
+
         // このバージョンがリリース版かスナップショットか
         public bool IsRelease;
 
         // Spigotとしてこのバージョンはありうるのか（ローカルにあるか否かは関係ない）
         public bool HasSpigot;
 
+        // server.jarのダウンロードurl
         private string DownloadURL;
+
         public VanillaVersion(string name, string downloadURL, bool isRelease, bool hasSpigot): base(name)
         {
             IsRelease = isRelease;
             HasSpigot = hasSpigot;
             DownloadURL = downloadURL;
         }
+
+        public override void SetNewVersion()
+        {
+            base.SetNewVersion();
+
+            logger.Info("Import vanila Server");
+
+            string url2;
+            try
+            {
+                //サーバーダウンロードのurlが記されたjsonをダウンロード
+                string new_jsonStr = wc.DownloadString(DownloadURL);
+
+                dynamic root2 = Newtonsoft.Json.JsonConvert.DeserializeObject(new_jsonStr);
+                url2 = root2.downloads.server.url;
+            }
+            catch (Exception ex)
+            {
+                string message =
+                        "Vanila サーバーのダウンロードに失敗しました。\n" +
+                        $"{Data_list.Version}はマルチサーバーが存在しない可能性があります。\n\n" +
+                        $"【エラー要因】\n{ex.Message}";
+                MW.MessageBox.Show(message, "Server Starter", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw new DownloadException($"Failed to get url to download server.jar (Error Message : {ex.Message})");
+            }
+
+            Directory.CreateDirectory(Path);
+
+            try
+            {
+                wc.DownloadFile(url2, $@"{Path}\server.jar");
+                wc.Dispose();
+            }
+            catch (Exception ex)
+            {
+                string message =
+                        "Vanila サーバーのダウンロードに失敗しました。\n\n" +
+                        $"【エラー要因】\n{ex.Message}";
+                MW.MessageBox.Show(message, "Server Starter", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw new DownloadException($"Failed to download server.jar (Error Message : {ex.Message})");
+            }
+
+            //一度実行し、eula.txtなどの必要ファイルを書き出す
+            Start_server(true);
+            MainWindow.Pd.Value = 15;
+            MainWindow.Pd.Message = "Output the server.jar, eula.txt and so on";
+
+            //eulaの書き換え
+            Change_eula();
+
+        }
     }
 
     public class SpigotVersion: Version
-    { 
+    {
+        public override string Path
+        {
+            get
+            {
+                return $@"{MainWindow.Data_Path}\Spigot_{Name}\";
+            }
+        }
+
+        // builderのダウンロードurl
         private string DownloadURL;
+
         public SpigotVersion(string name, string downloadURL) : base(name)
         {
             DownloadURL = downloadURL;
