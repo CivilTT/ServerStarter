7da454c5f0bef9f544277c88093749dd6d1790d0
CivilTT <civiltt.ss@gmail.com>
2022-01-23 12:06:51 +0900

    Remove Errors and Create DatapackTest

---
+++ UnitTestProject1/DatapackTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server_GUI2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class DatapackTest
+    {
+        [TestMethod]
+        public void ImportDp()
+        {
+            Version version = Version.TryGetInstance("1.16.5");
+            World world = World.TryGetInstance("TestWorld", version);
+            ImportDatapack datapack = ImportDatapack.TryGenInstance(world, "BoostMinecart", @"D:\Games\Minecraft\Datapacks\boost_minecart", false);
+            if (datapack == null)
+            {
+                System.Console.WriteLine("Invalid Datapack");
+                return;
+            }
+            datapack.Ready();
+        }
+    }
+}
