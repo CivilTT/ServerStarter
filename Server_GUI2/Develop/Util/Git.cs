5fd570727bac9b2b60cfdeb26e4daf68ceddadeb
txkodo <kodo.hsmkai@gmail.com>
2022-01-24 15:20:08 +0900

    changed Git

--- Server_GUI2/Develop/Util/Git.cs
+++ Server_GUI2/Develop/Util/Git.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Server_GUI2.Develop.Util
 {
-    class Git
+    class GitLocal
     {
+        public static GitLocal Clone(GitBranch branch, string path)
+        {
+            return new GitLocal(branch, path);
+        }
+
+        public string Path { get; private set;}
+        public GitBranch Branch { get; private set;}
+
+        public GitLocal(GitBranch branch, string path)
+        {
+            Path = path;
+            Branch = branch;
+        }
+
+        public void CheckOut(GitBranch branch)
+        {
+            Branch = branch;
+        }
+
+        public void push() { }
+        public void pull() { }
+    }
+
+    class GitRepository
+    {
+        public GitBranch GetBranch() { }
+    }
+
+    class GitBranch
+    {
+        public GitRepository Repository { get; private set; }
+        public GitBranch(GitRepository repository)
+        {
+            Repository = repository;
+        }
     }
 }
