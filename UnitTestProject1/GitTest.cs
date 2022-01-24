9f7ec1e564d6c67590495c9aa3ce13df23300ebe
txkodo <kodo.hsmkai@gmail.com>
2022-01-25 01:19:48 +0900

    fixed VersionFactory

--- UnitTestProject1/GitTest.cs
+++ UnitTestProject1/GitTest.cs
@@ -1,24 +1,24 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Server_GUI2.Develop.Util;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Server_GUI2.Develop.Util;
 
-namespace UnitTestProject1
-{
-    [TestClass]
-    public class GitTest
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var repo = new GitRemoteRepository("txkodo", "GitTest");
-            var branchs = repo.ExistsBranch();
+//namespace UnitTestProject1
+//{
+//    [TestClass]
+//    public class GitTest
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//            var repo = new GitRemoteRepository("txkodo", "GitTest");
+//            var branchs = repo.ExistsBranch();
 
-            var branch = branchs[1];
+//            var branch = branchs[1];
 
-            var local = new GitLocal(@"C:\Users\shiyu\OneDrive - 共生情報研究室\趣味\Git\ServerSterterTest", branch);
+//            var local = new GitLocal(@"C:\Users\shiyu\OneDrive - 共生情報研究室\趣味\Git\ServerSterterTest", branch);
 
-            local.InitIfIsNotRepository();
+//            local.InitIfIsNotRepository();
 
-            local.Pull();
-        }
-    }
-}
+//            local.Pull();
+//        }
+//    }
+//}
