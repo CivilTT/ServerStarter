f68ba38b83b553bade7c49e47c6a5eace583f2f9
txkodo <kodo.hsmkai@gmail.com>
2022-01-25 14:01:56 +0900

    changed GIt

--- Server_GUI2/Develop/Server/World/GitOperate.cs
+++ Server_GUI2/Develop/Server/World/GitOperate.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Server_GUI2;
 
 namespace Server_GUI2.Develop.Server.World
 {
+    /// <summary>
+    /// 各リポジトリの #state ブランチを確認する
+    /// </summary>
+    class GitState
+    {
+        SetUp.CurrentDirectory
+
+    }
     class GitOperate : Git
     {
     }
--- Server_GUI2/Develop/Util/Git.cs
+++ Server_GUI2/Develop/Util/Git.cs
@@ -1,185 +1,185 @@
-﻿//using log4net;
-//using System.Reflection;
-//using System;
-//using System.IO;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Diagnostics;
+﻿using log4net;
+using System.Reflection;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
-//namespace Server_GUI2.Develop.Util
-//{
-//    public class GitException : Exception
-//    {
-//        private readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+namespace Server_GUI2.Develop.Util
+{
+    public class GitException : Exception
+    {
+        private readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-//        public GitException(string message) : base(message)
-//        {
-//            logger.Info(message);
-//            Console.WriteLine(App.end_str);
-//        }
-//    }
+        public GitException(string message) : base(message)
+        {
+            logger.Info(message);
+            Console.WriteLine(App.end_str);
+        }
+    }
 
-//    class GitCommand
-//    {
-//        public static string ExecuteThrow(string path, string arguments, Exception exception)
-//        {
-//            var (code, output) = Execute(path, arguments);
+    class GitCommand
+    {
+        public static string ExecuteThrow(string path, string arguments, Exception exception)
+        {
+            var (code, output) = Execute(path, arguments);
 
-//            if (code != 0)
-//            {
-//                Console.WriteLine(code);
-//                Console.WriteLine(output);
-//                throw exception;
-//            }
-//            else
-//            {
-//                return output;
-//            }
-//        }
+            if (code != 0)
+            {
+                Console.WriteLine(code);
+                Console.WriteLine(output);
+                throw exception;
+            }
+            else
+            {
+                return output;
+            }
+        }
 
-//        public static (int, string) Execute(string path, string arguments)
-//        {
-//            Console.WriteLine($"-C \"{path}\" " + arguments);
-//            var process = new Process()
-//            {
-//                StartInfo = new ProcessStartInfo("git")
-//                {
-//                    Arguments = $"-C \"{path}\" " + arguments,
-//                    UseShellExecute = false,
-//                    RedirectStandardOutput = true,
-//                    StandardOutputEncoding = Encoding.UTF8
-//                }
-//            };
-//            process.Start();
-//            process.WaitForExit();
-//            string output = process.StandardOutput.ReadToEnd();
+        public static (int, string) Execute(string path, string arguments)
+        {
+            Console.WriteLine($"-C \"{path}\" " + arguments);
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo("git")
+                {
+                    Arguments = $"-C \"{path}\" " + arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    StandardOutputEncoding = Encoding.UTF8
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            string output = process.StandardOutput.ReadToEnd();
 
-//            return (process.ExitCode, output);
-//        }
-//    }
+            return (process.ExitCode, output);
+        }
+    }
 
 
-//    public class GitLocal
-//    {
-//        /// <summary>
-//        /// 与えられたパスがGitRepositoryのトップレベルかどうかを確認する
-//        /// </summary>
-//        public bool IsGitRepository()
-//        {
-//            var (code, output) = GitCommand.Execute(path, "rev-parse --show-toplevel");
-//            return code == 0 && Path.GetFullPath(output).Equals(Path.GetFullPath(path));
-//        }
+    public class GitLocal
+    {
+        /// <summary>
+        /// 与えられたパスがGitRepositoryのトップレベルかどうかを確認する
+        /// </summary>
+        public bool IsGitRepository()
+        {
+            var (code, output) = GitCommand.Execute(path, "rev-parse --show-toplevel");
+            return code == 0 && Path.GetFullPath(output).Equals(Path.GetFullPath(path));
+        }
 
-//        /// <summary>
-//        /// 現在のローカルブランチ名を取得する
-//        /// </summary>
-//        public string GetCurrentBranch()
-//        {
-//            var output = GitCommand.ExecuteThrow(path, "symbolic-ref --short HEAD", new GitException("cannot get current branch name."));
-//            return output.Substring(0, output.Length - 1); // 末尾の\nを削除
-//        }
+        /// <summary>
+        /// 現在のローカルブランチ名を取得する
+        /// </summary>
+        public string GetCurrentBranch()
+        {
+            var output = GitCommand.ExecuteThrow(path, "symbolic-ref --short HEAD", new GitException("cannot get current branch name."));
+            return output.Substring(0, output.Length - 1); // 末尾の\nを削除
+        }
 
-//        private string path;
-//        private GitRemoteBranch branch;
+        private string path;
+        private GitRemoteBranch branch;
 
-//        public GitLocal(string path)
-//        {
-//            this.path = path;
-//        }
+        public GitLocal(string path)
+        {
+            this.path = path;
+        }
 
-//        public void Init()
-//        {
-//            GitCommand.ExecuteThrow(path, $"init", new GitException($"failed to init {path}"));
-//        }
+        public void Init()
+        {
+            GitCommand.ExecuteThrow(path, $"init", new GitException($"failed to init {path}"));
+        }
 
-//        public void CommitPush(string commitMessage)
-//        {
-//            CommitAll(commitMessage);
-//            Push();
-//        }
+        public void CommitPush(string commitMessage)
+        {
+            CommitAll(commitMessage);
+            Push();
+        }
 
-//        public void Push(string localBranch, string remoteBranch)
-//        {
-//            GitCommand.ExecuteThrow(path, $"push {branch.Repository.Url} \"{branch.LocalBranchName}:{branch.Name}\"", new GitException($"failed to add branch {branch}"));
-//        }
+        public void Push(string localBranch, string remoteBranch)
+        {
+            GitCommand.ExecuteThrow(path, $"push {branch.Repository.Url} \"{branch.LocalBranchName}:{branch.Name}\"", new GitException($"failed to add branch {branch}"));
+        }
 
-//        public void CommitAll(string message)
-//        {
-//            GitCommand.ExecuteThrow(path, $"add -A", new GitException($"failed to add {path}"));
-//            GitCommand.ExecuteThrow(path, $"commit  -m \"{message}\"", new GitException($"failed to commit {path}"));
-//        }
+        public void CommitAll(string message)
+        {
+            GitCommand.ExecuteThrow(path, $"add -A", new GitException($"failed to add {path}"));
+            GitCommand.ExecuteThrow(path, $"commit  -m \"{message}\"", new GitException($"failed to commit {path}"));
+        }
 
-//        public void Pull()
-//        {
-//            GitCommand.ExecuteThrow(path, $"pull {branch.Repository.Url} \"{branch.Name}:{branch.LocalBranchName}\"", new GitException($"failed to get branch pull from {branch}"));
-//        }
-//    }
+        public void Pull()
+        {
+            GitCommand.ExecuteThrow(path, $"pull {branch.Repository.Url} \"{branch.Name}:{branch.LocalBranchName}\"", new GitException($"failed to get branch pull from {branch}"));
+        }
+    }
 
-//    public class GitRemoteRepository
-//    {
-//        public string Account { get; private set; }
-//        public string RepoName { get; private set; }
-//        public Dictionary<string, GitRemoteBranch> ExistsBranchMap { get; private set; }
-//        public string Url
-//        {
-//            get
-//            {
-//                return $"https://{Account}@github.com/{Account}/{RepoName}.git";
-//            }
-//        }
+    public class GitRemoteRepository
+    {
+        public string Account { get; private set; }
+        public string RepoName { get; private set; }
+        public Dictionary<string, GitRemoteBranch> ExistsBranchMap { get; private set; }
+        public string Url
+        {
+            get
+            {
+                return $"https://{Account}@github.com/{Account}/{RepoName}.git";
+            }
+        }
 
-//        public GitRemoteRepository(string account, string repoName)
-//        {
-//            Account = account;
-//            RepoName = repoName;
-//            GetExistsBranchs();
-//        }
+        public GitRemoteRepository(string account, string repoName)
+        {
+            Account = account;
+            RepoName = repoName;
+            GetExistsBranchs();
+        }
 
-//        private void GetExistsBranchs()
-//        {
-//            var output = GitCommand.ExecuteThrow("/", $"ls-remote --heads {Url}", new GitException($"failed to get branch list from {Url}"));
+        private void GetExistsBranchs()
+        {
+            var output = GitCommand.ExecuteThrow("/", $"ls-remote --heads {Url}", new GitException($"failed to get branch list from {Url}"));
 
-//            var lines = output.Substring(0, output.Length - 1).Split('\n');
-//            foreach (var i in lines)
-//            {
-//                if (i.Length == 0)
-//                {
-//                    continue;
-//                }
-//                var branchName = i.Substring(52);
-//                ExistsBranchMap[branchName] = new GitRemoteBranch(this, branchName);
-//            }
-//        }
+            var lines = output.Substring(0, output.Length - 1).Split('\n');
+            foreach (var i in lines)
+            {
+                if (i.Length == 0)
+                {
+                    continue;
+                }
+                var branchName = i.Substring(52);
+                ExistsBranchMap[branchName] = new GitRemoteBranch(this, branchName);
+            }
+        }
 
-//        public GitRemoteBranch NewBranch(string branchName)
-//        {
+        public GitRemoteBranch NewBranch(string branchName)
+        {
 
-//        }
-//    }
+        }
+    }
 
-//    public class GitRemoteBranch
-//    {
-//        public GitRemoteRepository Repository { get; private set; }
-//        public string Name { get; private set; }
+    public class GitRemoteBranch
+    {
+        public GitRemoteRepository Repository { get; private set; }
+        public string Name { get; private set; }
 
-//        public string LocalBranchName
-//        {
-//            get
-//            {
-//                return $"{this.Repository.Account}.{this.Repository.RepoName}.{this.Name}";
-//            }
-//        }
+        public string LocalBranchName
+        {
+            get
+            {
+                return $"{this.Repository.Account}.{this.Repository.RepoName}.{this.Name}";
+            }
+        }
 
-//        public GitRemoteBranch(GitRemoteRepository repository, string name)
-//        {
-//            Repository = repository;
-//            Name = name;
-//        }
-//        public override string ToString()
-//        {
-//            return $"<GitRemoteBranch {Repository.Url} {Name} >";
-//        }
-//    }
-//}
+        public GitRemoteBranch(GitRemoteRepository repository, string name)
+        {
+            Repository = repository;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return $"<GitRemoteBranch {Repository.Url} {Name} >";
+        }
+    }
+}
