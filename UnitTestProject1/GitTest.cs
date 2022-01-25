4669eb9555c3e066a8d873ececa08e87d6824848
txkodo <kodo.hsmkai@gmail.com>
2022-01-25 21:29:30 +0900

    Created GitTest

---
+++ UnitTestProject1/GitTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server_GUI2;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Windows.Data;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class GitTest
+    {
+        [TestMethod]
+        public void GitTestMethod()
+        { 
+        }
+    }
+}
