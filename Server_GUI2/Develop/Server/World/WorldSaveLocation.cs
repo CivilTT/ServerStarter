a4fd922b3a0227b6413a0ceed50432777fda0be5
txkodo <kodo.hsmkai@gmail.com>
2022-01-26 21:20:53 +0900

    created WorldSaveLocation

---
+++ Server_GUI2/Develop/Server/World/WorldSaveLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_GUI2.Develop.Server.World
+{
+    class Class1
+    {
+    }
+}
