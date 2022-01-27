424df5941f2b5c8c4c7ad40db8313c78e1feca89
txkodo <kodo.hsmkai@gmail.com>
2022-01-27 23:36:37 +0900

    + Storage StorageFactory

--- Server_GUI2/Develop/Server/Storage/Storage.cs
+++ Server_GUI2/Develop/Server/Storage/Storage.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Server_GUI2.Develop.Server.World;
 
-namespace Server_GUI2.Develop.Server.World
+namespace Server_GUI2.Develop.Server.Storage
 {
     /// <summary>
     /// ワールドの保存先(ローカルフォルダ,Gitリポジトリ,Gdrive等)
     /// </summary>
-    public abstract class WorldSaveLocation
+    public abstract class Storage
     {
         public abstract WorldWriter GetWorldWriter(Version version);
     }
 
-    class LocalWorldSaveLocation : WorldSaveLocation
+    class LocalWorldSaveLocation : Storage
     {
         public override WorldWriter GetWorldWriter(Version version)
         {
             return new LocalWorldWriter();
         }
     }
-    class GitWorldSaveLocation : WorldSaveLocation
+    class GitWorldSaveLocation : Storage
     {
         public override WorldWriter GetWorldWriter(Version version)
         {
---
+++ Server_GUI2/Develop/Server/Storage/StorageFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_GUI2.Develop.Server.Storage
+{
+    class StorageFactory
+    {
+        public StorageFactory Instance { get; } = new StorageFactory();
+
+        public ObservableCollection<Storage> Storages { get; } = new ObservableCollection<Storage>();
+        private StorageFactory()
+        {
+
+        }
+    }
+}
