46d7409a004179fd40d5036b3129ce9dbd48628c
CivilTT <civiltt.ss@gmail.com>
2022-02-06 23:59:06 +0900

    Created World Settings Window

--- Server_GUI2/Windows/MoreSettings/WorldSettingsVM.cs
+++ Server_GUI2/Windows/MoreSettings/WorldSettingsVM.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace Server_GUI2.Windows.MoreSettings
 {
@@ -21,22 +22,30 @@ namespace Server_GUI2.Windows.MoreSettings
         public int MenuIndex { get; set; } = 0;
         //public bool ShowProp { get { return MenuIndex == 0; } }
         //public bool ShowSW { get { return MenuIndex == 1; } }
-        //public bool ShowDatapck { get { return MenuIndex == 2; } }
-        //public bool ShowPlugin { get { return MenuIndex == 3; } }
-        //public bool ShowMap { get { return MenuIndex == 4; } }
-        //public bool ShowOp { get { return MenuIndex == 5; } }
-        //public bool ShowWhite { get { return MenuIndex == 6; } }
+        //public bool ShowAdd { get { return MenuIndex == 2; } }
+        //public bool ShowOp { get { return MenuIndex == 3; } }
+        //public bool ShowWhite { get { return MenuIndex == 4; } }
 
         // GUI作成中用
         // 表示が重複してしまった場合、一度ビルドすれば直る
         public bool ShowProp { get { return false; } }
         public bool ShowSW { get { return false; } }
-        public bool ShowDatapck { get { return false; } }
-        public bool ShowPlugin { get { return false; } }
-        public bool ShowMap { get { return false; } }
+        public bool ShowAdd { get { return false; } }
         public bool ShowOp { get { return false; } }
         public bool ShowWhite { get { return false; } }
 
 
+        //Op
+        public ObservableCollection<Player> OpPlayersList = new ObservableCollection<Player>();
+
+
+    }
+
+    public class Player
+    {
+        public string Name;
+        public string UUID;
+        public int OpLevel;
+        public bool BypassesPlayerLimit = false;
     }
 }
