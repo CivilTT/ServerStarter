65a74a4bfd769418d9d1b286cfcd452ef5cc25b2
CivilTT <civiltt.ss@gmail.com>
2022-02-08 09:09:35 +0900

    Recreate WorldSettings

--- Server_GUI2/Windows/MoreSettings/WorldSettingsVM.cs
+++ Server_GUI2/Windows/MoreSettings/WorldSettingsVM.cs
@@ -20,19 +20,19 @@ namespace Server_GUI2.Windows.MoreSettings
 
         // 設定項目の表示非表示を操作
         public int MenuIndex { get; set; } = 0;
-        //public bool ShowProp { get { return MenuIndex == 0; } }
-        //public bool ShowSW { get { return MenuIndex == 1; } }
-        //public bool ShowAdd { get { return MenuIndex == 2; } }
-        //public bool ShowOp { get { return MenuIndex == 3; } }
-        //public bool ShowWhite { get { return MenuIndex == 4; } }
+        public bool ShowProp { get { return MenuIndex == 0; } }
+        public bool ShowSW { get { return MenuIndex == 1; } }
+        public bool ShowAdd { get { return MenuIndex == 2; } }
+        public bool ShowOp { get { return MenuIndex == 3; } }
+        public bool ShowWhite { get { return MenuIndex == 4; } }
 
         // GUI作成中用
         // 表示が重複してしまった場合、一度ビルドすれば直る
-        public bool ShowProp { get { return false; } }
-        public bool ShowSW { get { return false; } }
-        public bool ShowAdd { get { return false; } }
-        public bool ShowOp { get { return false; } }
-        public bool ShowWhite { get { return false; } }
+        //public bool ShowProp { get { return false; } }
+        //public bool ShowSW { get { return false; } }
+        //public bool ShowAdd { get { return false; } }
+        //public bool ShowOp { get { return false; } }
+        //public bool ShowWhite { get { return false; } }
 
 
         //Op
