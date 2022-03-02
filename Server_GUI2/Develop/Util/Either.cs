eec6f9349be95f88a6bc68c9e3f8789d1b245bad
txkodo <kodo.hsmkai@gmail.com>
2022-03-03 02:43:42 +0900

    added EitherTest

--- Server_GUI2/Develop/Util/Either.cs
+++ Server_GUI2/Develop/Util/Either.cs
@@ -10,9 +10,13 @@ namespace Server_GUI2.Develop.Util
     {
         public abstract bool IsSuccess { get; }
 
-        public abstract Either<T, FAILURE> SuccessMap<T>(Func<SUCCESS, T> func);
+        public abstract Either<T, FAILURE> SuccessFunc<T>(Func<SUCCESS, T> func);
 
-        public abstract Either<SUCCESS, T> FailureMap<T>(Func<FAILURE, T> func);
+        public abstract Either<SUCCESS, T> FailureFunc<T>(Func<FAILURE, T> func);
+        
+        public abstract void SuccessAction(Action<SUCCESS> action);
+
+        public abstract void FailureAction(Action<FAILURE> action);
 
         public abstract SUCCESS SuccessOrDefault(SUCCESS defaultValue);
 
@@ -30,13 +34,17 @@ namespace Server_GUI2.Develop.Util
             Value = value;
         }
 
-        public override Either<T, FAILURE> SuccessMap<T>(Func<SUCCESS, T> func) => new Success<T, FAILURE>(func(Value));
+        public override Either<T, FAILURE> SuccessFunc<T>(Func<SUCCESS, T> func) => new Success<T, FAILURE>(func(Value));
 
-        public override Either<SUCCESS, T> FailureMap<T>(Func<FAILURE, T> func) => new Success<SUCCESS, T>(Value);
+        public override Either<SUCCESS, T> FailureFunc<T>(Func<FAILURE, T> func) => new Success<SUCCESS, T>(Value);
 
         public override SUCCESS SuccessOrDefault(SUCCESS defaultValue) => Value;
 
         public override FAILURE FailureOrDefault(FAILURE defaultValue) => defaultValue;
+
+        public override void SuccessAction(Action<SUCCESS> action) { action(Value); }
+
+        public override void FailureAction(Action<FAILURE> action) { }
     }
 
     public class Failure<SUCCESS, FAILURE> : Either<SUCCESS, FAILURE>
@@ -50,13 +58,17 @@ namespace Server_GUI2.Develop.Util
             Value = value;
         }
 
-        public override Either<T, FAILURE> SuccessMap<T>(Func<SUCCESS, T> func) => new Failure<T, FAILURE>(Value);
+        public override Either<T, FAILURE> SuccessFunc<T>(Func<SUCCESS, T> func) => new Failure<T, FAILURE>(Value);
 
-        public override Either<SUCCESS, T> FailureMap<T>(Func<FAILURE, T> func) => new Failure<SUCCESS, T>(func(Value));
+        public override Either<SUCCESS, T> FailureFunc<T>(Func<FAILURE, T> func) => new Failure<SUCCESS, T>(func(Value));
 
         public override SUCCESS SuccessOrDefault(SUCCESS defaultValue) => defaultValue;
 
         public override FAILURE FailureOrDefault(FAILURE defaultValue) => Value;
+
+        public override void SuccessAction(Action<SUCCESS> action) { }
+
+        public override void FailureAction(Action<FAILURE> action) { action(Value); }
 
     }
 }
