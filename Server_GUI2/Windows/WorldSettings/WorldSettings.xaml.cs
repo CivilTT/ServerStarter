fe7334895e504f2411282b9b97e41fa0553e8c70
CivilTT <civiltt.ss@gmail.com>
2022-03-18 21:47:20 +0900

    Fixed Internet Connection Test and the dialog visible setting in WorldSettings window

--- Server_GUI2/Windows/WorldSettings/WorldSettings.xaml.cs
+++ Server_GUI2/Windows/WorldSettings/WorldSettings.xaml.cs
@@ -39,6 +39,9 @@ namespace Server_GUI2.Windows.WorldSettings
                 if (result == MessageBoxResult.No)
                     e.Cancel = true;
             }
+
+            // ShowDialog中にさらにShowDialogすると、最後のDialogが終了した際に元の画面とその子画面が両方表示されるため、これを避けるためにここでShowし、Main側に再表示の機能を持たせない
+            Owner.Show();
         }
     }
 }
