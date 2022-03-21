bb1ffdde1c6f45cd05b0f8dd77b1cca816b13b8c
CivilTT <civiltt.ss@gmail.com>
2022-03-21 23:50:44 +0900

    Binded custom Message Box

--- Server_GUI2/Windows/MessageBox/MessageBox.xaml.cs
+++ Server_GUI2/Windows/MessageBox/MessageBox.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -17,11 +16,53 @@ namespace Server_GUI2.Windows.MessageBox
     /// <summary>
     /// MessageBox.xaml の相互作用ロジック
     /// </summary>
-    public partial class MessageBox : Window
+    public partial class MessageBox : GeneralCB
     {
         public MessageBox()
         {
             InitializeComponent();
+        }
+    }
+
+    public static class CustomMessageBox
+    {
+        public static string Show(string message, ButtonType type, Image icon)
+        {
+            string[] buttons;
+            switch (type)
+            {
+                case ButtonType.OK:
+                    buttons = new string[1] { "OK" };
+                    break;
+                case ButtonType.OKCancel:
+                    buttons = new string[2] { "OK", "Cancel" };
+                    break;
+                case ButtonType.YesNo:
+                    buttons = new string[2] { "Yes", "No" };
+                    break;
+                default:
+                    throw new ArgumentException("This is unknown type");
+            }
+
+            return Show(message, buttons, icon);
+        }
+        public static string Show(string message, string[] buttons, Image icon)
+        {
+            return Show(message, buttons, icon, new LinkMessage("", ""));
+        }
+
+        public static string Show(string message, string[] buttons, Image icon, LinkMessage link)
+        {
+            return Show(message, "Server Starter", buttons, icon, link);
+        }
+
+        public static string Show(string message, string title, string[] buttons, Image icon, LinkMessage link)
+        {
+            var window = new ShowNewWindow<MessageBox, MessageBoxVM>();
+            var vm = new MessageBoxVM(message, title, buttons, icon, link);
+            window.ShowDialog(vm);
+
+            return buttons[vm.SelectedIndex];
         }
     }
 }
