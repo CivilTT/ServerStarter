dde827c938695fd4c6ee347179dce934f96b2c53
CivilTT <civiltt.ss@gmail.com>
2022-05-27 15:32:28 +0900

    Fixed the message of unknown Issue

--- Server_GUI2/Develop/Util/Git.cs
+++ Server_GUI2/Develop/Util/Git.cs
@@ -207,7 +207,6 @@ namespace Server_GUI2.Util
                     RedirectStandardError = true,
                     RedirectStandardOutput = true,
                     WorkingDirectory = Path,
-                    // TODO: @CivilTT '"'の削除し、'\'を'/'に置換
                     Arguments = $"/s /c \"cmd /q /c if @fsize GTR 100000000 echo @relpath\""
                 };
                 process.Start();
