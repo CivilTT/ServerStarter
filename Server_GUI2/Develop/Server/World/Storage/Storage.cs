f9b9901aa76163cc84cc9ba6a08b3c9e646bbc4c
CivilTT <civiltt.ss@gmail.com>
2022-06-12 20:08:18 +0900

    Fixed the repo and branch selection bug in WorldSettings

--- Server_GUI2/Develop/Server/World/Storage/Storage.cs
+++ Server_GUI2/Develop/Server/World/Storage/Storage.cs
@@ -102,7 +102,7 @@ namespace Server_GUI2.Develop.Server.World
 
         public virtual void AddWorld(RemoteWorld world)
         {
-            RemoteWorlds.Add(world);
+            RemoteWorlds.Insert(RemoteWorlds.Count-2, world);
             world.DeleteEvent += new EventHandler((_, __) => RemoteWorlds.Remove(world));
         }
 
