7d31fa9ca1794d775d8a1688d5b682c8641bb28e
txkodo <kodo.hsmkai@gmail.com>
2022-07-09 00:23:44 +0900

    set h1~h3 default style

--- src/components/MainWindow/Start.vue
+++ src/components/MainWindow/Start.vue
@@ -1,15 +1,16 @@
 <template>
   <q-card flat style="max-width: 100%;">
     <q-card-section>
-      <h1>サーバーの起動</h1>
-      以下の画面が本システムのメイン画面です。
-      Versionにて起動するサーバーのバージョンを指定し、Worldにて起動するワールドを選択します。
-      最初はVersionもWordも何もインストールされていないため、【new Version(World)】より、インストールしたいバージョンを選択し、起動するワールドの名前を決めてください。
-
-      ワールドの諸設定は「ワールド設定」から行ってください．
-      準備ができたら「実行」を押してサーバーを起動しましょう！<br/>
-
-      <img alt="main img" src="../../assets/MainWindow/main1.png" style="height: 15cm;"/>
+      <h1 class="title">サーバーの起動</h1>
+      <p>
+        以下の画面が本システムのメイン画面です。
+        Versionにて起動するサーバーのバージョンを指定し、Worldにて起動するワールドを選択します。
+        最初はVersionもWordも何もインストールされていないため、【new Version(World)】より、インストールしたいバージョンを選択し、起動するワールドの名前を決めてください。
+  
+        ワールドの諸設定は「ワールド設定」から行ってください．
+        準備ができたら「実行」を押してサーバーを起動しましょう！
+      </p>
+      <img alt="main img" src="../../assets/MainWindow/main1.png" style="width: 100%;"/>
     </q-card-section>
   </q-card>
 </template>
