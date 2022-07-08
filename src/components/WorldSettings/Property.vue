8aa85d8166a3466d1051d0516ecd2f2190e35a69
CivilTT <civiltt.ss@gmail.com>
2022-07-09 00:15:41 +0900

    Added <p> tag

--- src/components/WorldSettings/Property.vue
+++ src/components/WorldSettings/Property.vue
@@ -2,10 +2,12 @@
   <q-card flat style="max-width: 100%;">
     <q-card-section>
       <h1>サーバープロパティ</h1>
-      ServerStarterではサーバープロパティを各ワールドごとに設定することができます．
-      「適用」を押すことで<router-link to="/system">システム設定</router-link>にて設定したデフォルトプロパティが反映されます
-      「リセット」を押すことで<router-link to="/system">システム設定</router-link>にて設定したプロパティに戻すことができます
-      設定が終わったところで「保存」を押すと設定が反映されます
+      <p>
+        ServerStarterではサーバープロパティを各ワールドごとに設定することができます．
+        「適用」を押すことで<router-link to="/system">システム設定</router-link>にて設定したデフォルトプロパティが反映されます
+        「リセット」を押すことで<router-link to="/system">システム設定</router-link>にて設定したプロパティに戻すことができます
+        設定が終わったところで「保存」を押すと設定が反映されます
+      </p>
     </q-card-section>
   </q-card>
 </template>
