0d8f01e5267c5bc007990f01df214e102fd04e3d
txkodo <kodo.hsmkai@gmail.com>
2022-08-09 18:27:40 +0900

    起動時にgit pullに失敗する問題を修正

--- Server_GUI2/Develop/Server/World/Storage/GitOperate.cs
+++ Server_GUI2/Develop/Server/World/Storage/GitOperate.cs
@@ -87,7 +87,8 @@ namespace Server_GUI2.Develop.Server.World
             logger.Info("<ReadWorldState>");
             return GetRemoteBranch(remote).SuccessFunc(branch =>
             {
-                branch.CommitPush("checkout other branch");
+                // git pullに失敗するためコメントアウト何か別の問題が起こるかも
+                // branch.CommitPush("checkout other branch");
                 branch.LocalBranch.Checkout();
                 branch.Pull();
                 var worldState = stateDirectory.WorldStateJson.ReadJson().SuccessOrDefault(new Dictionary<string, WorldState>());
