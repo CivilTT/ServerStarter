245372bc929e61aa4dd43251a39a8ed3c872f5d3
txkodo <kodo.hsmkai@gamil.com>
2022-12-11 19:00:55 +0900

    urlを直接たたいた時に404にならないようにする

---
+++ config/index.js
@@ -0,0 +1,12 @@
+'use strict'
+// Template version: 1.2.5
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+
+module.exports = {
+  build: {
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsPublicPath: '',
+  }
+}
