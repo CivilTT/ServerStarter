d3c5f8ddae233595e88201a1aa04367b8ed0f582
CivilTT <civiltt.ss@gmail.com>
2023-01-03 19:37:27 +0900

    ✨ Added contents in APM

--- src/components/AutoPortMapping/How.vue
+++ src/components/AutoPortMapping/How.vue
@@ -3,11 +3,22 @@
     <q-card-section>
       <h1>何をすればよいの？</h1>
       <p>
-        ポート開放するものは主に「ルーター」と「セキュリティソフト」になります。
-        ポートはアプリケーションごとに開放すべきポートがある程度決まっており、Minecraftにおいては「25565」番を解放します
-        <!-- TODO: 自動ポート開放へのリンクをつける -->
-        ルーターについては「<a href="" class="a">自動ポート開放</a>」を利用できますので、ご利用をご検討ください。
+        ポート開放するものは主に「ルーター」と「セキュリティソフト」になります
       </p>
+      <p>
+        ポートはアプリケーションごとに開放すべきポートがある程度決まっており、Minecraftにおいては「25565」番を解放します
+      </p>
+      <p>
+        ポート開放を行うことでLAN内のサーバーと通信することができます
+      </p>
+      <q-img src="@/assets/AutoPortMapping/Abstruct2.png" width="min(800px,100%)" class="img"/>
     </q-card-section>
   </q-card>
 </template>
+
+<style scoped>
+.img {
+  display: block;
+  margin : 0 auto;
+}
+</style>
--- src/components/AutoPortMapping/Settings.vue
+++ src/components/AutoPortMapping/Settings.vue
@@ -1,25 +1,66 @@
+<script lang="ts" setup>
+
+const columns = [
+  {
+    name: 'index',
+    label: '設定項目名',
+    field: 'index',
+    style: 'font-size: 1rem'
+  },
+  {
+    name: 'value',
+    label: '設定値',
+    field: 'value',
+    style: 'font-size: 1rem'
+  }
+]
+
+const rows = [
+  {
+    index: '外部からのアクセス',
+    value: '（全てのPCに対して）許可'
+  },
+  {
+    index: 'ポートの種類',
+    value: 'TCP'
+  },
+  {
+    index: 'LAN側IPアドレス',
+    value: '(192.168.で始まるIPアドレス)'
+  },
+  {
+    index: 'ポート番号',
+    value: '25565 - (25565)'
+  },
+]
+
+</script>
+
 <template>
   <q-card flat style="max-width: 100%;">
     <q-card-section>
       <h1>設定項目</h1>
       <p>
-        各ルーターやセキュリティソフトごとに細かい設定内容や説明文の表現方法が異なりますので、ここでは一般的な設定項目について示します
-        詳細な設定方法については各メーカーの取扱説明書をご参照下さい。
+        各ルーターやセキュリティソフトごとに細かい設定内容や説明文の表現方法が異なります<br/>
+        ここでは一般的な設定項目について示しますが，詳細な設定方法については各メーカーの取扱説明書をご参照下さい
       </p>
       <ol>
         <li>ルーターやセキュリティソフトの設定画面を開く</li>
         <li>「ファイアウォール」や「セキュリティー」の設定画面を開く</li>
         <li>「ルールの追加」や「ポートの追加」を開く</li>
         <li>各設定項目を次のように設定する</li>
-        <ul>
-          <li>外部からのアクセス　（全てのPCに対して）許可</li>
-          <li>ポートの種類　TCP</li>
-          <li>LAN側IPアドレス　ServerStarterを起動し、システム設定のNetworkタブに記載されています</li>
-          <li>ポート番号　　25565（-25565）</li>
-        </ul>
+        <div style="width: min(600px, 100%);" class="q-pl-md">
+          <q-table
+            :rows="rows"
+            :columns="columns"
+            hide-bottom
+          />
+        </div>
         <li>設定を保存し、サーバーにほかの人がログインできれば設定終了</li>
-        ※一人でポート開放の確認を行う場合は、サーバーが起動した後に<a href="https://www.cman.jp/network/support/port.html" class="a">株式会社シーマンが運営するこちらのHP</a>にアクセスすることで確認することができます
       </ol>
+      <p>
+        ※一人でポート開放の確認を行う場合は、サーバー起動後に<a href="https://www.cman.jp/network/support/port.html" class="a">こちらのHP</a>にアクセスすることで確認できます
+      </p>
     </q-card-section>
   </q-card>
 </template>
