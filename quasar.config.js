2fb176fc271abe503a819b63e5f6a49a8e392d58
CivilTT <civiltt.ss@gmail.com>
2023-08-27 01:41:54 +0900

    ✨ Supported qdialog

--- quasar.config.js
+++ quasar.config.js
@@ -115,7 +115,9 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Dialog'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
