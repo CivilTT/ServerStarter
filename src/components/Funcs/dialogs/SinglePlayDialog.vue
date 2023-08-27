27840ee54b6282a6ddefa176628404b2b01e4fbc
CivilTT <civiltt.ss@gmail.com>
2023-08-27 11:56:03 +0900

    ✨ Added SinglePlayDialog

---
+++ src/components/Funcs/dialogs/SinglePlayDialog.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useDialogPluginComponent } from 'quasar';
+import BaseDialogCard from './BaseDialogCard.vue';
+
+defineEmits({...useDialogPluginComponent.emitsObject})
+const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent()
+</script>
+
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <BaseDialogCard
+      title="個人ワールドをみんなで遊ぶ"
+      @close="onDialogOK"
+    >
+      <ul>
+        <li>一人で遊んでいたワールドをみんなとシェアしたい</li>
+        <li>ワールド製作をみんなに協力してもらいたい</li>
+      </ul>
+
+      <p>
+        ServerStarterでは個人ワールドを簡単にマルチプレイ化する機能を搭載！<br>
+        ワールドの共有に難しい操作は必要ありません！
+      </p>
+
+      <template #install>
+        <ol>
+          <li>ホームタブにある「既存ワールドを選択」ボタンをクリック</li>
+          <!-- 操作画面の画像を入れる -->
+          <li>シングルプレイのワールド一覧から好きなワールドを選択するだけ！</li>
+          <!-- 操作画面の画像を入れる -->
+        </ol>
+      </template>
+    </BaseDialogCard>
+  </q-dialog>
+</template>
--- src/pages/FuncsView.vue
+++ src/pages/FuncsView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import FuncItem from 'src/components/Funcs/FuncItem.vue';
 import MultipleServersDialog from 'src/components/Funcs/dialogs/MultipleServersDialog.vue';
+import SinglePlayDialog from 'src/components/Funcs/dialogs/SinglePlayDialog.vue';
 
 </script>
 
@@ -8,45 +9,51 @@ import MultipleServersDialog from 'src/components/Funcs/dialogs/MultipleServersD
   <h1 class="q-pl-md">主要機能一覧</h1>
 
   <div class="row q-gutter-md q-pa-md justify-center">
-    <FuncItem title="様々なサーバー" asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
-      :dialog-component="MultipleServersDialog">
-      起動可能なサーバーは全部で６種類です
-    </FuncItem>
-    <!-- <FuncItem
-      title="個人ワールドにご招待"
+    <!-- アイコンを6つ並べた画像を表示？ -->
+    <FuncItem
+      title="様々なサーバー"
       asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
+      :dialog-component="MultipleServersDialog"
     >
-      シングルプレイワールドをマルチプレイ化することができます
+      厳選した6種類のサーバーが様々な遊び方をサポートします
     </FuncItem>
     
     <FuncItem
+      title="個人ワールドにご招待"
+      asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
+      :dialog-component="SinglePlayDialog"
+    >
+      シングルプレイワールドをマルチプレイ化することができます
+    </FuncItem>
+
+    <!-- <FuncItem
       title="分かりやすい設定画面"
       asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
     >
       各種設定、データパックなどの視覚的な操作
-    </FuncItem>
+    </FuncItem> -->
 
-    <FuncItem
+    <!-- <FuncItem
       title="保存場所は自由自在"
       asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
     >
-      
-    </FuncItem>
-    
-    <FuncItem
+
+    </FuncItem> -->
+
+    <!-- <FuncItem
       title="プレイヤー権限設定"
       asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
     >
-    </FuncItem>
+    </FuncItem> -->
 
-    <FuncItem
+    <!-- <FuncItem
       title="ワールドを複数人で共有"
       asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
     >
       ShareWorldタブに飛ぶ
-    </FuncItem>
+    </FuncItem> -->
 
-    <FuncItem
+    <!-- <FuncItem
       title="サーバーの顔"
       asset-path="https://cdn.quasar.dev/img/parallax2.jpg"
     >
