7324626481e54fa5db047e934f8d9eacc97fc027
CivilTT <civiltt.ss@gmail.com>
2023-08-27 22:58:38 +0900

    Updates

--- assets/i18n.ae9bd114.js
+++ assets/i18n.ae9bd114.js
@@ -1,4 +1,4 @@
-import{h as Xe,e as wt,i as Ut,o as Wt,a as Vt,r as Q,c as q,w as ce,F as xt,g as Ye,b as jt,d as Gt,T as $t,f as Ht}from"./index.fb862ff1.js";/*!
+import{h as Xe,e as wt,i as Ut,o as Wt,a as Vt,r as Q,c as q,w as ce,F as xt,g as Ye,b as jt,d as Gt,T as $t,f as Ht}from"./index.af4bdcf0.js";/*!
   * shared v9.2.2
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
